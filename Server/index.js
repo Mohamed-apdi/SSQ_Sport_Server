b87ee128a7ba5b3cd56e50d2a601cd1a7684b4d0
mohamed <maxamedcabdifitaax33@gmail.com>
2024-12-21 14:54:43 +0300

    fixed

--- Server/index.js
+++ Server/index.js
@@ -7,29 +7,13 @@ import cors from "cors";
 
 const app = express();
 
-const corsOptions = {
-    origin: 'http://localhost:5173', // Allow your Vite development server
-    credentials: true, // Allow cookies and credentials
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-};
 
-app.use(cors(corsOptions));
-
-// Preflight request handling
-app.options('*', cors(corsOptions));
-
+app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", userRouter);
 app.use("/api/product", productRoute);
-
-app.use((req, res, next) => {
-    console.log('Request Origin:', req.headers.origin);
-    next();
-});
-
 
 dbConnection();
 
