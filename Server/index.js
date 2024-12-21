bbee82adef81ada6b14073eb7e21a3e20e341ee1
mohamed <maxamedcabdifitaax33@gmail.com>
2024-12-21 12:26:02 +0300

    fixed proxy

--- Server/index.js
+++ Server/index.js
@@ -7,8 +7,13 @@ import cors from "cors";
 
 const app = express();
 
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", userRouter);
