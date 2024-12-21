3df135e99b7472082a90dc7ce0e62c6e2e5c3bf7
mohamed <maxamedcabdifitaax33@gmail.com>
2024-12-21 12:26:48 +0300

    fixed proxy

--- Server/index.js
+++ Server/index.js
@@ -14,6 +14,7 @@ const corsOptions ={
  }
 
 app.use(cors(corsOptions));
+app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", userRouter);
