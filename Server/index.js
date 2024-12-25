ee770d645a5fde845a0a9856bf2583100f78c243
mohamed <maxamedcabdifitaax33@gmail.com>
2024-12-25 19:23:41 +0300

    cors fixed

--- Server/index.js
+++ Server/index.js
@@ -6,12 +6,15 @@ import { productRoute } from './routers/product.js';
 import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:5173", // Replace with your React app's URL
+  credentials: true, // Allow credentials (cookies, authorization headers)
+};
 
-
-app.use(cors());
 app.use(express.json())
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/user", userRouter);
 app.use("/api/product", productRoute);
 
